75cd02ec645dfe11fb107aee10460030733d11b4
Anggoran <anggoranm@gmail.com>
2023-03-29 16:29:13 +0700

    fresh hyperlink

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,18 @@
 export default function Footer() {
   return (
     <footer className="flex justify-center pt-16 pb-4">
-      <img
-        className="select-none"
-        width="150"
-        src="https://fresh.deno.dev/fresh-badge-dark.svg"
-        alt="Made with Fresh"
-      />
+      <a
+        href="https://fresh.deno.dev"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          className="select-none"
+          width="150"
+          src="https://fresh.deno.dev/fresh-badge-dark.svg"
+          alt="Made with Fresh"
+        />
+      </a>
     </footer>
   );
 }
